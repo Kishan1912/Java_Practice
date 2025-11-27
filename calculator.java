fb59bf17bfa43330b8a5f681bff3fb03f1367c74
Kishan1912 <118465341+Kishan1912@users.noreply.github.com>
2025-11-27 10:59:18 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class calculator {
+    static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+        int a = n;
+        int b = ;
+        int sum = a+b;
+        System.out.println(sum);
+
+
+    }
+}
