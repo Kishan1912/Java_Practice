da0f9f3b255b669a925b43786075e0e5556d7415
Kishan1912 <118465341+Kishan1912@users.noreply.github.com>
2025-11-28 13:28:28 +0530

    Add files via upload

---
+++ week.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class week{
+}
+
+ public static void main(String[] args) {
+    Scanner Sc = new Scanner(System.in);
+    int n= Sc.nextInt();
+
+    switch (n){
+        case 1 : System.out.println("Monday");
+        break;
+        case 2 : System.out.println("Tuesday");
+        break;
+        case 3 : System.out.println("Wednesday");
+        break;
+        case 4: System.out.println("thursday");
+        break;
+        case 5: System.out.println("Friday");
+        break;
+        case 6: System.out.println("Saturday");
+        break;
+        default: System.out.println("Sunday");
+    }
+
+
+}
