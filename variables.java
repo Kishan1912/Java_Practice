43b3194bbbb680165af568679bbec800c674128d
Kishan1912 <118465341+Kishan1912@users.noreply.github.com>
2025-11-28 13:29:10 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables{
+    static void main(String[] args) {
+        //variables
+      //  String name="kishan yadav";
+      //  int age =24;
+      //  double price = 25.35;
+       // int a =39;
+        //int b =47;
+      //  System.out.println(a);
+        int a= 10;
+        int b= 20;
+        int sum = a+b;
+       // System.out.println(sum);
+        int mul = a*b;
+        System.out.println(mul);
+
+
+
+
+    }
+}
