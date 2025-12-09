befca43ba6aa8c70d72400b756e17aa6562bb177
Kishan1912 <118465341+Kishan1912@users.noreply.github.com>
2025-12-09 18:46:18 +0530

    Add files via upload

---
+++ TwoDArrays.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TwoDArrays {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int rows = Sc.nextInt();
+        int cols = Sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+        //input
+        //rows
+        for (int i = 0; i < rows; i++) {
+            //cols
+            for (int j = 0; j < cols; j++) {
+                numbers[i][j] = Sc.nextInt();
+            }
+        }
+        //output
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
