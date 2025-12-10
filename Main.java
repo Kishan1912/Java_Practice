65abd005be95543e1a5da706aa4c742bc4d2ff92
Kishan1912 <118465341+Kishan1912@users.noreply.github.com>
2025-12-10 18:27:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int number = Sc.nextInt();
+
+
+        if (number%2==0){
+            System.out.println("even");
+
+        }else
+            System.out.println("odd");
+
+
+        }
+    }
